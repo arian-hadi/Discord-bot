272e2e439f63abd72ccfb3a01121e0b77626a482
Arian Hadi <arianhadi2003@gmail.com>
2024-07-11 22:12:57 +0300

    kick feature

--- main.py
+++ main.py
@@ -5,41 +5,16 @@ from apikey import *
 import requests,json,asyncio
 import json
 import datetime
-
+from googleapiclient.discovery import build
 
 intents = discord.Intents.all()
 intents.members = True
 client = commands.Bot(command_prefix = '!', intents=intents)
 
-@client.command()
-async def hello (ctx):
-    await ctx.send("Hello I am your Discord bot")
 
-@client.command()
-async def bye (ctx):
-    await ctx.send("Goodbye! take care!")
-
-@client.event
-async def on_member_join(member):
-    channel = client.get_channel(channel_id)
-    await channel.send(f"{member.mention}")
-    myembed = discord.Embed(
-        title = "Welcome to the server",
-        description=f"| Check Out Other Channels to get started!! |\n <#1252240278257926207>",
-        color =0x5900b3
-        )
-    myembed.set_author(name = member.display_name, icon_url = member.display_avatar.url)
-    myembed.set_thumbnail(url = member.display_avatar)
-    myembed.add_field(name="\u200b", value="❄ ENJOY YOUR STAY IN THE SERVER :DD ❄", inline=False)
-    myembed.set_image(url=gif)
-    await channel.send(embed=myembed)
-    
-
-@client.event
-async def on_member_remove(member):
-    channel = client.get_channel(channel_id)
-    await channel.send(f"Goodbye {member}!", )
-
+def timestamp():
+    current_time = datetime.datetime.utcnow().strftime("Date = %Y-%m-%d || Time = %H:%M:%S UTC")
+    return current_time
 
 def get_api():
     response = requests.get(url)
@@ -74,23 +49,72 @@ def check_new_video():
     except Exception as e:
         print(f"An error occured, type error: {e}")
 
+
+youtube = build("youtube", "v3", developerKey=API_KEY)
+def get_pfp(youtube_id):
+    request = youtube.channels().list(
+        part = "snippet",
+        id = youtube_id
+    )
+    response = request.execute()
+    channel_pfp = response['items'][0]['snippet']['thumbnails']['default']['url']
+    return channel_pfp
+
+
+@client.command()
+async def hello (ctx):
+    await ctx.send("Hello I am your Discord bot")
+
+@client.command()
+async def bye (ctx):
+    await ctx.send("Goodbye! take care!")
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(channel_id)
+    await channel.send(f"{member.mention}")
+    myembed = discord.Embed(
+        title = "Welcome to the server",
+        description=f"| Check Out Other Channels to get started!! |\n <#1252240278257926207>",
+        color =0x5900b3
+        )
+    myembed.set_author(name = member.display_name, icon_url = member.display_avatar.url)
+    myembed.set_thumbnail(url = member.display_avatar)
+    myembed.add_field(name="\u200b", value="❄ ENJOY YOUR STAY IN THE SERVER :DD ❄", inline=False)
+    myembed.set_image(url=gif)
+    await channel.send(embed=myembed)
+    
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(channel_id)
+    await channel.send(f"Goodbye {member}!", )
+
+
 @tasks.loop(seconds=30.0)  # adjust this as needed
 async def check_new_videos():
     channel = client.get_channel(channel_id)  # replace with your YouTube channel ID
     message, id_video,channel_title,caption = check_new_video()
+    #hyperlink_format = f'<a href="{message}">{caption}</a>'
+    #link_text = hyperlink_format.format
     print ("checking....")
     await asyncio.sleep(5)
     print(id_video)
     if message is None:
         return None
     result = get_api()
-    channel_pfp = result['items'][0]['snippet']['thumbnails']['default']['url']
+    channel_thumbnail = result['items'][0]['snippet']['thumbnails']['default']['url']
+    channel_pfp = get_pfp(YOUTUBE_ID)
+    embed = discord.Embed(
+        title = f"[{caption}({message})]",
+        description= f"{channel_title} published a video on YouTube!",
+        color = discord.Color.red()
+    )
+    embed.set_author(name = channel_title, icon_url = channel_pfp)
+    embed.set_thumbnail(url = channel_pfp)
+    embed.set_image(url = channel_thumbnail)
     await channel.send(f"@everyone {channel_title} shared a new video: ' {caption}'\n <{message}>",)
-
-
-def timestamp():
-    current_time = datetime.datetime.utcnow().strftime("Date = %Y-%m-%d || Time = %H:%M:%S UTC")
-    return current_time
+    await channel.send(embed = embed)
 #Loggin edited message
 @client.event
 async def on_message_edit(before, after):
@@ -109,7 +133,7 @@ async def on_message_edit(before, after):
 
     await channel.send(embed = embed)
 
-#Loggin delted message
+#Loggin deleted message
 @client.event
 async def on_message_delete(message):
     channel = client.get_channel(1252240278257926207)
@@ -160,7 +184,21 @@ async def on_member_update(before, after):
     embed.set_author(name = after.name, icon_url = after.display_avatar.url)
     await channel.send(embed = embed)
 
-
+#kick command
+@client.command()
+@commands.has_permissions(kick_members = True)
+async def kick(ctx, member:discord.Member,*,reason = None):
+    channel = client.get_channel(1252240278257926207)
+    current_time = timestamp()
+    try:
+        await member.kick(reason = reason)
+        embed = discord.Embed(color = discord.Color.red(),title = "", description= "")
+        embed.add_field(name ="Kicked", value =f"""The user  **{member}** has been kicked.\nReason = **{reason}**""", inline = True)
+        embed.set_author(name = str(member), icon_url = member.display_avatar.url)
+        embed.set_footer(text= current_time)
+        await channel.send(embed = embed)
+    except:
+         await channel.send("Something went wrong")
 
 @client.event
 async def on_ready():
