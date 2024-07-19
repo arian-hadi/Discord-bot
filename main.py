513ee0dd4c70d7b20580b5c4339cf402bf2f0d40
Aizen <147944755+hollow12sk@users.noreply.github.com>
2024-07-19 20:35:12 +0300

    Self roles updated
    Update 1:
    -self roles
    -SSL fixes
    -Bug fixes

--- main.py
+++ main.py
@@ -8,59 +8,13 @@ from discord.ext.commands import has_permissions, MissingPermissions
 import json
 import asyncio
 
-
 intents = discord.Intents.all()
-intents.members = True
-client = commands.Bot(command_prefix = '!', intents=intents)
-intents = discord.Intents.default()
-intents.message_content = True
-intents.reactions = True
-intents.guilds = True
 
+client = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
 @client.event
 async def on_ready():
-    await client.tree.sync()
-    print ("The bot is now ready for use")
-    print("------------------------------")
-
-@client.command()
-async def hello (ctx):
-    await ctx.send("Hello I am your Discord bot")
-
-@client.command()
-async def bye (ctx):
-    await ctx.send("Goodbye! take care!")
-
-@client.event
-async def on_member_join(member):
-    channel = client.get_channel(channel_id)
-    await channel.send(f"{member.mention}")
-    myembed = discord.Embed(
-        title = "Welcome to the server",
-        description=f"| Check Out Other Channels to get started!! |\n <#1252240278257926207>",
-        color =0x5900b3
-        )
-    myembed.set_author(name = member.display_name, icon_url = member.display_avatar.url)
-    myembed.set_thumbnail(url = member.display_avatar)
-    myembed.add_field(name="\u200b", value="❄ ENJOY YOUR STAY IN THE SERVER :DD ❄", inline=False)
-    myembed.set_image(url=gif)
-    await channel.send(embed=myembed)
-    
-
-@client.event
-async def on_member_remove(member):
-    channel = client.get_channel(channel_id)
-    await channel.send(f"Goodbye {member}!", )
-    
-@client.command(pass_context = True)
-async def join(ctx):
-    pass
-
-#Self roles section
-async def on_ready():
-    print('system rebboted!')
-    client.add_view(Roles())
+    print('System rebooted')
 
 class Roles(discord.ui.View):
     def __init__(self):
@@ -119,9 +73,7 @@ class Roles(discord.ui.View):
 @client.command()
 async def roles(ctx):
     embed = discord.Embed(title = 'Role selection form.', description = 'Get your desired role!, press to add/Remove role!')
-    await ctx.send(embed =  embed, view = Roles())  
-
-
+    await ctx.send(embed =  embed, view = Roles())                                      
 client.run('MTI1MTg0ODk5MzY5NzE3MzYyNg.GauxMO.-URD6QWnU-dnQ1ZnTWDb39WkWVKFYdc9MN0uTs')
 
 
